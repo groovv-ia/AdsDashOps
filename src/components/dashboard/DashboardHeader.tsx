b80707a509ada922818fc9f320d7ae4832bf23e3
Groovv <groovv.ia@gmail.com>
2025-07-01 14:06:45 -0300

    Remover funcionalidade de FAQ do header

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -4,7 +4,6 @@ import { Button } from '../ui/Button';
 import { NotificationBell } from '../notifications/NotificationBell';
 import { NotificationCenter } from '../notifications/NotificationCenter';
 import { NotificationSettingsModal } from '../notifications/NotificationSettings';
-import { HelpButton } from '../help/HelpButton';
 import { signOut } from '../../lib/supabase';
 import { User as UserType } from '@supabase/supabase-js';
 import { supabase } from '../../lib/supabase';
@@ -123,7 +122,6 @@ export const DashboardHeader: React.FC<DashboardHeaderProps> = ({
           </div>
 
           <div className="flex items-center space-x-2 lg:space-x-3">
-            <HelpButton />
             <NotificationBell onClick={() => setNotificationCenterOpen(true)} />
 
             <div className="relative">
